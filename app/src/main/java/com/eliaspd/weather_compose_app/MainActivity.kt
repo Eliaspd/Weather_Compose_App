a6e5e53ccf876e31f0a6e565c38ead8d333b9fe3
Eliaspd <swedetailer@gmail.com>
2024-07-29 19:55:37 +0200

    first commit

---
+++ app/src/main/java/com/eliaspd/weather_compose_app/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.eliaspd.weather_compose_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.eliaspd.weather_compose_app.ui.theme.Weather_Compose_AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Weather_Compose_AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WeatherPage()
+                }
+            }
+        }
+    }
+}
